301b5aabf30b5ec6372a795478ae468822daca8f
zilvis565-dev <zilvis565@gmail.com>
2026-09-23 17:17:12 +0300

    Update print statement from 'Hello' to 'Goodbye'

--- app.py
+++ app.py
@@ -683,8 +683,8 @@ def export_order(order_id):
     return send_file(filename, as_attachment=True)
 
 
-
+# IMPORTANT for Render / Waitress
+init_db()
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0", port=5000)
-
